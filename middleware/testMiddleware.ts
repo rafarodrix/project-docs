e5e8547cb750271e8836ff8c84e41653dcc03467
rafael Rodrigues <rafaelrodrix@gmail.com>
2024-09-21 11:25:04 -0300

    Ajustes

--- middleware/testMiddleware.ts
+++
@@ -1,7 +0,0 @@
-// middleware/testMiddleware.ts
-import { Context } from '@nuxt/types'
-
-export default function ({ redirect }: Context) {
-  console.log('Middleware de teste executado') // Verifique se este log aparece no terminal
-  return redirect('/login') // Redireciona para a página de login para testar o middleware
-}
